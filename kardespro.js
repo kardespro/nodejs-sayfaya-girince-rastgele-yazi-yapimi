9ccd0ce5c810d3fd3358f2205246d23cc7d78f35
kardespro <61694826+kardespro@users.noreply.github.com>
2020-10-23 21:30:01 +0400

    kardespro.js
    cf.kardespro.github
    Siteme Uğramayı Unutmayın

---
+++ kardespro.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const ejs = require("ejs");
+const ejs-loader = require("ejs-loader");
+const app = express();
+
+//===============================
+//       kardespro.cf
+//    Daha Fazla Bilgiyi Burdan al
+//    info@kardespro.cf
+//    Author:kardespro
+//    Lisance:MIT
+//=================================
+
+app.get('/api/rastgele', function(req,res){
+var kardespro = ["kardespro","kraldir","gerisi","yalandir","xd","cf.kardespro.github","kardespro.cf","abiiii"]; //kelimeler
+var result = []; // Cevaplari belirtelim
+
+for (var i=0; i<2; i++){
+var number = Math.floor(Math.random()*kardespro.length);
+var kardes = number;
+res.json(`$kardes`);
+//http://localhost:3000/api/rastgele
+//GLITCH.COM-DA ÇALIŞIR
+//KENDINIZE GORE APILER YAPA BILIRSINIZ
+});
+
